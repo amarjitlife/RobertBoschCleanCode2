3fdcb1a56c5ced79ff72a9a05886ef61e5740d45
Amarjit Singh <amarjit.developer@gmail.com>
2025-09-23 17:29:59 +0530

    Create Assignments.c

---
+++ Assignments.c
@@ -0,0 +1,14 @@
+__________________________________________________
+
+DAY 02
+__________________________________________________
+
+	ASSIGNMENT A1: Practice Ideas and Code
+
+	ASSIGNMENT A2: Reading and Practice Assignment
+		Read and Practice Code Examples From Following Chapters
+			Chapter 10: Better Structures
+			Chatper 11: Object Oriented Programming In C
+	
+			Reference Book: 21st Century C, 2nd Edition
+__________________________________________________
